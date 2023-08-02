c56f5b2de6c323a64e246aecef4156e227f856dd
Gazal Gahlyan <90563771+GazalGahlyan@users.noreply.github.com>
2023-08-02 09:02:21 +0530

    Update securityGp.tf

--- securityGp.tf
+++ securityGp.tf
@@ -1,1 +1,20 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+data "aws_security_groups" "all_security_groups" {
+  tags = {
+    Terraform = "true"
+  }
+}
+
+data "aws_security_group" "exposed_port_22" {
+  count = length(data.aws_security_groups.all_security_groups.ids)
+
+  id = data.aws_security_groups.all_security_groups.ids[count.index]
+}
+
+output "exposed_security_groups" {
+  value = [for sg in data.aws_security_group.exposed_port_22 : sg.tags.Name]
+}
 
